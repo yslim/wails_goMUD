c93ffc6e823eb834cb646dc36c1abdcd428407d3
yslim <yslim@mulbul.com>
2022-12-11 21:50:21 +0900

    add event OnReady

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,71 +1,72 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
-import { debounceTime, fromEvent, map, startWith, Subscription } from "rxjs";
-import { Send } from "../../../wailsjs/go/service/MudService";
-import { EventsOn } from "../../../wailsjs/runtime";
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Terminal} from "xterm";
+import {FitAddon} from "xterm-addon-fit";
+import {debounceTime, fromEvent, map, startWith, Subscription} from "rxjs";
+import {Send} from "../../../wailsjs/go/service/MudService";
+import {EventsOn, EventsEmit} from "../../../wailsjs/runtime";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
-    @ViewChild('term') terminalElement!: ElementRef;
-    inputBuffer = "";
-    terminal: Terminal | undefined;
-    termFit: FitAddon | undefined;
-    windowResizeSubs: Subscription | undefined;
+  @ViewChild('term') terminalElement!: ElementRef;
+  inputBuffer = "";
+  terminal: Terminal | undefined;
+  termFit: FitAddon | undefined;
+  windowResizeSubs: Subscription | undefined;
 
-    ngOnInit(): void {
-        this.windowResizeSubs = windowSizeObserver().subscribe(({width, height}) => {
-            this.termFit?.fit();
-        })
+  ngOnInit(): void {
+    this.windowResizeSubs = windowSizeObserver().subscribe(({width, height}) => {
+      this.termFit?.fit();
+    })
 
-        EventsOn("OnMessage", (message: string) => {
-            this.terminal?.write(message);
-        });
+    EventsOn("OnMessage", (message: string) => {
+      this.terminal?.write(message);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.windowResizeSubs?.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    // init terminal & addons
+    this.terminal = new Terminal({
+      allowProposedApi: true,
+      fontFamily: "Gulimche",
+      fontSize: 17,
+    });
+    this.termFit = new FitAddon();
+    this.terminal.loadAddon(this.termFit);
+
+    // open terminal in DOM
+    this.terminal.open(this.terminalElement.nativeElement);
+    this.termFit.fit();
+
+    this.terminal.onData((data) => {
+      this.terminal!.write(data);
+    });
+
+    EventsEmit("OnReady");
+  }
+
+  onKeyDown($event: KeyboardEvent) {
+    if ($event.key === "Enter") {
+      Send(this.inputBuffer);
+      this.inputBuffer = "";
     }
-
-    ngOnDestroy(): void {
-        this.windowResizeSubs?.unsubscribe();
-    }
-
-    ngAfterViewInit(): void {
-        // init terminal & addons
-        this.terminal = new Terminal({
-            allowProposedApi: true,
-            fontFamily: "Gulimche",
-            fontSize: 17,
-        });
-        this.termFit = new FitAddon();
-        this.terminal.loadAddon(this.termFit);
-
-        // open terminal in DOM
-        this.terminal.open(this.terminalElement.nativeElement);
-        this.termFit.fit();
-
-        this.terminal.onData((data) => {
-            this.terminal!.write(data);
-        });
-        0
-    }
-
-    onKeyDown($event: KeyboardEvent) {
-        if ($event.key === "Enter") {
-            Send(this.inputBuffer);
-            this.inputBuffer = "";
-        }
-    }
+  }
 }
 
 function windowSizeObserver(dTime = 300) {
-    return fromEvent(window, 'resize').pipe(
-        debounceTime(dTime),
-        map(event => {
-            const window = event.target as Window;
-            return {width: window.innerWidth, height: window.innerHeight}
-        }),
-        startWith({width: window.innerWidth, height: window.innerHeight})
-    );
+  return fromEvent(window, 'resize').pipe(
+    debounceTime(dTime),
+    map(event => {
+      const window = event.target as Window;
+      return {width: window.innerWidth, height: window.innerHeight}
+    }),
+    startWith({width: window.innerWidth, height: window.innerHeight})
+  );
 }
